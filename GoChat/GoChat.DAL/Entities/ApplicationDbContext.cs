6fdf9ec167a74e01b63fe8baa9a8fcea2bda3c8d
Islam Umarov <fpm.umarov@bsu.by>
2019-09-30 17:08:34 +0300

    dadas

--- GoChat/GoChat.DAL/Entities/ApplicationDbContext.cs
+++ GoChat/GoChat.DAL/Entities/ApplicationDbContext.cs
@@ -17,31 +17,6 @@ namespace GoChat.DAL.Entities
         {
             optionsBuilder.UseMySql(GetConnectionString());
         }
-      
-      
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-
-            // Shorten key length for Identity
-            builder.Entity<AppUser>(entity => entity.Property(m => m.Id).HasMaxLength(127));
-            //builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(127));
-            builder.Entity<IdentityUserLogin<string>>(entity =>
-            {
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.ProviderKey).HasMaxLength(127);
-            });
-            builder.Entity<IdentityUserToken<string>>(entity =>
-            {
-                entity.Property(m => m.UserId).HasMaxLength(127);
-                entity.Property(m => m.LoginProvider).HasMaxLength(127);
-                entity.Property(m => m.Name).HasMaxLength(127);
-
-            });
-        }
 
         private static string GetConnectionString()
         {
