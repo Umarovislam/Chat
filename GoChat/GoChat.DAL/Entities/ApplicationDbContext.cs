f1f4727b685d6205d964449b5864dfc8159c175c
Islam Umarov <fpm.umarov@bsu.by>
2019-09-30 16:29:31 +0300

    asa

--- GoChat/GoChat.DAL/Entities/ApplicationDbContext.cs
+++ GoChat/GoChat.DAL/Entities/ApplicationDbContext.cs
@@ -1,19 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.EntityFrameworkCore;
 
 namespace GoChat.DAL.Entities
 {
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(GetConnectionString());
         }
-
+      
         public DbSet<AppUser> Users;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            // Shorten key length for Identity
+            builder.Entity<AppUser>(entity => entity.Property(m => m.Id).HasMaxLength(127));
+            //builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(127));
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.ProviderKey).HasMaxLength(127);
+            });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.Name).HasMaxLength(127);
+
+            });
+        }
+
         private static string GetConnectionString()
         {
             const string databaseName = "gochatdb";
