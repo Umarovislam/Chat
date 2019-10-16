1f6386ba3e682e6cb18d92a17407f79dadf90e32
Islam Umarov <fpm.umarov@bsu.by>
2019-10-16 11:56:41 +0300

    sdf

--- Go-Chat-Front/src/app/shared/services/signal-r.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import * as signalR from '@aspnet/signalr';
-
-
-// let connection: HubConnectionBuilder;
-// csonnection = new signalR.HubConnectionBuilder();
-export class SignalRService {
-  constructor() { }
-}
